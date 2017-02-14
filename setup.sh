5c3d81cfcb7585d3cf1df25841d84b7443832362
romeo-golf <romeo-golf@users.noreply.github.com>
2017-02-14 13:28:05 +0000

    Harmonize variables and comments with ADSB Receiver script

--- setup.sh
+++ setup.sh
@@ -5,7 +5,7 @@
 #####################################################################################
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 #                                                                                   #
-# Copyright (c) 2015-2016 Joseph A. Prochazka                                       #
+# Copyright (c) 2015-2017 Joseph A. Prochazka                                       #
 #                                                                                   #
 # Permission is hereby granted, free of charge, to any person obtaining a copy      #
 # of this software and associated documentation files (the "Software"), to deal     #
@@ -27,6 +27,14 @@
 #                                                                                   #
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 
+### VARIABLES
+
+RECEIVER_PROJECT_TITLE="ADS-B Exchange Setup Script"
+LOG_DIRECTORY="$PWD/logs"
+MLAT_CLIENT_VERSION="0.2.6"
+MLAT_CLIENT_TAG="v0.2.6"
+
+### BEGIN SETUP
 
 ## CHECK IF SCRIPT WAS RAN USING SUDO
 
@@ -50,116 +58,119 @@ if [ $(dpkg-query -W -f='${STATUS}' curl 2>/dev/null | grep -c "ok installed") -
 fi
 echo -e "\033[37m"
 
-## ASSIGN VARIABLES
-
-LOGDIRECTORY="$PWD/logs"
-MLATCLIENTVERSION="0.2.6"
-MLATCLIENTTAG="v0.2.6"
-
 ## WHIPTAIL DIALOGS
 
-BACKTITLETEXT="ADS-B Exchange Setup Script"
-
-whiptail --backtitle "$BACKTITLETEXT" --title "$BACKTITLETEXT" --yesno "Thanks for choosing to share your data with ADS-B Exchange!\n\nADSBexchange.com is a co-op of ADS-B/Mode S/MLAT feeders from around the world. This script will configure your current your ADS-B receiver to share your feeders data with ADS-B Exchange.\n\nWould you like to continue setup?" 13 78
-CONTINUESETUP=$?
-if [ $CONTINUESETUP = 1 ]; then
+# Interactive install.
+whiptail --backtitle "$RECEIVER_PROJECT_TITLE" --title "$RECEIVER_PROJECT_TITLE" --yesno "Thanks for choosing to share your data with ADS-B Exchange!\n\nADSBexchange.com is a co-op of ADS-B/Mode S/MLAT feeders from around the world. This script will configure your current your ADS-B receiver to share your feeders data with ADS-B Exchange.\n\nWould you like to continue setup?" 13 78
+CONTINUE_SETUP=$?
+if [ $CONTINUE_SETUP = 1 ]; then
     exit 0
 fi
 
-ADSBEXCHANGEUSERNAME=$(whiptail --backtitle "$BACKTITLETEXT" --title "ADS-B Exchange User Name" --nocancel --inputbox "\nPlease enter your ADS-B Exchange user name.\n\nIf you have more than one receiver, this username should be unique.\nExample: \"username-01\", \"username-02\", etc." 12 78 3>&1 1>&2 2>&3)
-RECEIVERLATITUDE=$(whiptail --backtitle "$BACKTITLETEXT" --title "Receiver Latitude" --nocancel --inputbox "\nPlease enter your receiver's latitude." 9 78 3>&1 1>&2 2>&3)
-RECEIVERLONGITUDE=$(whiptail --backtitle "$BACKTITLETEXT" --title "Receiver Longitude" --nocancel --inputbox "\nPlease enter your receiver's longitude." 9 78 3>&1 1>&2 2>&3)
-RECEIVERALTITUDE=$(whiptail --backtitle "$BACKTITLETEXT" --title "Receiver Altitude" --nocancel --inputbox "\nPlease enter your receiver's altitude in meters above sea level, the below value is obtained from Google but should be increased to reflect your antennas height above ground level." 11  78 "`curl -s https://maps.googleapis.com/maps/api/elevation/json?locations=$RECEIVERLATITUDE,$RECEIVERLONGITUDE | python -c "import json,sys;obj=json.load(sys.stdin);print obj['results'][0]['elevation'];"`" 3>&1 1>&2 2>&3)
+## CHECK FOR PREREQUISITE PACKAGES
 
-whiptail --backtitle "$BACKTITLETEXT" --title "$BACKTITLETEXT" --yesno "We are now ready to begin setting up your receiver to feed ADS-B Exchange.\n\nDo you wish to proceed?" 9 78
-CONTINUESETUP=$?
-if [ $CONTINUESETUP = 1 ]; then
+## CONFIRM DERIVED VALUES
+
+# For interactive install we test each required variable and prompt the user if not present.
+ADSBEXCHANGE_USERNAME=$(whiptail --backtitle "$RECEIVER_PROJECT_TITLE" --title "ADS-B Exchange User Name" --nocancel --inputbox "\nPlease enter your ADS-B Exchange user name.\n\nIf you have more than one receiver, this username should be unique.\nExample: \"username-01\", \"username-02\", etc." 12 78 3>&1 1>&2 2>&3)
+RECEIVER_LATITUDE=$(whiptail --backtitle "$RECEIVER_PROJECT_TITLE" --title "Receiver Latitude" --nocancel --inputbox "\nPlease enter your receiver's latitude." 9 78 3>&1 1>&2 2>&3)
+RECEIVER_LONGITUDE=$(whiptail --backtitle "$RECEIVER_PROJECT_TITLE" --title "Receiver Longitude" --nocancel --inputbox "\nPlease enter your receiver's longitude." 9 78 3>&1 1>&2 2>&3)
+RECEIVER_ALTITUDE=$(whiptail --backtitle "$RECEIVER_PROJECT_TITLE" --title "Receiver Altitude" --nocancel --inputbox "\nPlease enter your receiver's altitude in meters above sea level, the below value is obtained from Google but should be increased to reflect your antennas height above ground level." 11  78 "`curl -s https://maps.googleapis.com/maps/api/elevation/json?locations=$RECEIVER_LATITUDE,$RECEIVER_LONGITUDE | python -c "import json,sys;obj=json.load(sys.stdin);print obj['results'][0]['elevation'];"`" 3>&1 1>&2 2>&3)
+
+whiptail --backtitle "$RECEIVER_PROJECT_TITLE" --title "$RECEIVER_PROJECT_TITLE" --yesno "We are now ready to begin setting up your receiver to feed ADS-B Exchange.\n\nDo you wish to proceed?" 9 78
+CONTINUE_SETUP=$?
+if [ $CONTINUE_SETUP = 1 ]; then
     exit 0
 fi
 
-## BEGIN SETUP
+## START WHIPTAIL PROGRESS GAUGE
 
 {
 
     # Make a log directory if it does not already exist.
-    if [ ! -d "$LOGDIRECTORY" ]; then
-        mkdir $LOGDIRECTORY
+    if [ ! -d "$LOG_DIRECTORY" ]; then
+        mkdir $LOG_DIRECTORY
     fi
-    LOGFILE="$LOGDIRECTORY/image_setup-$(date +%F_%R)"
-    touch $LOGFILE
+    LOG_FILE="$LOG_DIRECTORY/image_setup-$(date +%F_%R)"
+    touch $LOG_FILE
 
     echo 4
     sleep 0.25
 
-    # BUILD AND CONFIGURE THE MLAT-CLIENT PACKAGE
-
-    echo "INSTALLING PREREQUISITE PACKAGES" >> $LOGFILE
-    echo "--------------------------------------" >> $LOGFILE
-    echo "" >> $LOGFILE
+    echo "INSTALLING PREREQUISITE PACKAGES" >> $LOG_FILE
+    echo "--------------------------------------" >> $LOG_FILE
+    echo "" >> $LOG_FILE
 
 
     # Check that the prerequisite packages needed to build and install mlat-client are installed.
     if [ $(dpkg-query -W -f='${STATUS}' build-essential 2>/dev/null | grep -c "ok installed") -eq 0 ]; then
-        sudo apt-get install -y build-essential >> $LOGFILE  2>&1
+        sudo apt-get install -y build-essential >> $LOG_FILE  2>&1
     fi
 
     echo 10
     sleep 0.25
 
     if [ $(dpkg-query -W -f='${STATUS}' debhelper 2>/dev/null | grep -c "ok installed") -eq 0 ]; then
-        sudo apt-get install -y debhelper >> $LOGFILE  2>&1
+        sudo apt-get install -y debhelper >> $LOG_FILE  2>&1
     fi
 
     echo 16
     sleep 0.25
 
     if [ $(dpkg-query -W -f='${STATUS}' python3-dev 2>/dev/null | grep -c "ok installed") -eq 0 ]; then
-        sudo apt-get install -y python3-dev >> $LOGFILE  2>&1
+        sudo apt-get install -y python3-dev >> $LOG_FILE  2>&1
     fi
 
     echo 22
     sleep 0.25
 
     if [ $(dpkg-query -W -f='${STATUS}' netcat 2>/dev/null | grep -c "ok installed") -eq 0 ]; then
-        sudo apt-get install -y netcat >> $LOGFILE  2>&1
+        sudo apt-get install -y netcat >> $LOG_FILE  2>&1
     fi
 
     echo 28
     sleep 0.25
 
-    echo "" >> $LOGFILE
-    echo " BUILD AND INSTALL MLAT-CLIENT" >> $LOGFILE
-    echo "-----------------------------------" >> $LOGFILE
-    echo "" >> $LOGFILE
+    ## DOWNLOAD OR UPDATE THE MLAT-CLIENT SOURCE
 
-    # Check if the mlat-client git repository already exists.
+    echo "" >> $LOG_FILE
+    echo " BUILD AND INSTALL MLAT-CLIENT" >> $LOG_FILE
+    echo "-----------------------------------" >> $LOG_FILE
+    echo "" >> $LOG_FILE
+
+    # Check if build directory exists and contains a git repository.
     if [ -d mlat-client ] && [ -d mlat-client/.git ]; then
-        # If the mlat-client repository exists update the source code contained within it.
-        cd mlat-client >> $LOGFILE
-        git pull >> $LOGFILE 2>&1
-        git checkout tags/$MLATCLIENTTAG >> $LOGFILE 2>&1
+        # A directory with a git repository containing the source code already exists.
+        cd mlat-client >> $LOG_FILE
+        git pull >> $LOG_FILE 2>&1
+        git checkout tags/$MLAT_CLIENT_TAG >> $LOG_FILE 2>&1
     else
-        # Download a copy of the mlat-client repository since the repository does not exist locally.
-        git clone https://github.com/mutability/mlat-client.git >> $LOGFILE 2>&1
-        cd mlat-client >> $LOGFILE 2>&1
-        git checkout tags/$MLATCLIENTTAG >> $LOGFILE 2>&1
+        # A directory containing the source code does not exist locally.
+        git clone https://github.com/mutability/mlat-client.git >> $LOG_FILE 2>&1
+        cd mlat-client >> $LOG_FILE 2>&1
+        git checkout tags/$MLAT_CLIENT_TAG >> $LOG_FILE 2>&1
     fi
 
     echo 34
     sleep 0.25
 
-    # Build and install the mlat-client package.
-    dpkg-buildpackage -b -uc >> $LOGFILE 2>&1
-    cd .. >> $LOGFILE
-    sudo dpkg -i mlat-client_${MLATCLIENTVERSION}*.deb >> $LOGFILE
+    ## BUILD AND INSTALL THE MLAT-CLIENT PACKAGE
+
+    # Build binary package.
+    dpkg-buildpackage -b -uc >> $LOG_FILE 2>&1
+    cd .. >> $LOG_FILE
+    # Install binary package.
+    sudo dpkg -i mlat-client_${MLAT_CLIENT_VERSION}*.deb >> $LOG_FILE
 
     echo 40
     sleep 0.25
 
-    echo "" >> $LOGFILE
-    echo " CREATE AND CONFIGURE MLAT-CLIENT STARTUP SCRIPTS" >> $LOGFILE
-    echo "------------------------------------------------------" >> $LOGFILE
-    echo "" >> $LOGFILE
+    ## CREATE THE SCRIPT TO EXECUTE AND MAINTAIN NETCAT AND MLAT-CLIENT FEEDS ADS-B EXCHANGE
+
+    echo "" >> $LOG_FILE
+    echo " CREATE AND CONFIGURE MLAT-CLIENT STARTUP SCRIPTS" >> $LOG_FILE
+    echo "------------------------------------------------------" >> $LOG_FILE
+    echo "" >> $LOG_FILE
 
     # Create the mlat-client maintenance script.
     tee adsbexchange-mlat_maint.sh > /dev/null <<EOF
@@ -167,7 +178,7 @@ fi
 while true
   do
     sleep 30
-    /usr/bin/mlat-client --input-type dump1090 --input-connect localhost:30005 --lat $RECEIVERLATITUDE --lon $RECEIVERLONGITUDE --alt $RECEIVERALTITUDE --user $ADSBEXCHANGEUSERNAME --server feed.adsbexchange.com:31090 --no-udp --results beast,connect,localhost:30104
+    /usr/bin/mlat-client --input-type dump1090 --input-connect localhost:30005 --lat $RECEIVER_LATITUDE --lon $RECEIVER_LONGITUDE --alt $RECEIVER_ALTITUDE --user $ADSBEXCHANGE_USERNAME --server feed.adsbexchange.com:31090 --no-udp --results beast,connect,localhost:30104
   done
 EOF
 
@@ -175,7 +186,7 @@ EOF
     sleep 0.25
 
     # Set execute permissions on the mlat-client maintenance script.
-    chmod +x adsbexchange-mlat_maint.sh >> $LOGFILE
+    chmod +x adsbexchange-mlat_maint.sh >> $LOG_FILE
 
     echo 52
     sleep 0.25
@@ -183,29 +194,29 @@ EOF
     # Add a line to execute the mlat-client maintenance script to /etc/rc.local so it is started after each reboot if one does not already exist.
     if ! grep -Fxq "$PWD/adsbexchange-mlat_maint.sh &" /etc/rc.local; then
         LINENUMBER=($(sed -n '/exit 0/=' /etc/rc.local))
-        ((LINENUMBER>0)) && sudo sed -i "${LINENUMBER[$((${#LINENUMBER[@]}-1))]}i $PWD/adsbexchange-mlat_maint.sh &\n" /etc/rc.local >> $LOGFILE
+        ((LINENUMBER>0)) && sudo sed -i "${LINENUMBER[$((${#LINENUMBER[@]}-1))]}i $PWD/adsbexchange-mlat_maint.sh &\n" /etc/rc.local >> $LOG_FILE
     fi
 
     echo 58
     sleep 0.25
 
-    echo "" >> $LOGFILE
-    echo " CREATE AND CONFIGURE NETCAT STARTUP SCRIPTS" >> $LOGFILE
-    echo "-------------------------------------------------" >> $LOGFILE
-    echo "" >> $LOGFILE
+    echo "" >> $LOG_FILE
+    echo " CREATE AND CONFIGURE NETCAT STARTUP SCRIPTS" >> $LOG_FILE
+    echo "-------------------------------------------------" >> $LOG_FILE
+    echo "" >> $LOG_FILE
 
     # Kill any currently running instances of the adsbexchange-mlat_maint.sh script.
     PIDS=`ps -efww | grep -w "adsbexchange-mlat_maint.sh" | awk -vpid=$$ '$2 != pid { print $2 }'`
     if [ ! -z "$PIDS" ]; then
-        sudo kill $PIDS >> $LOGFILE
-        sudo kill -9 $PIDS >> $LOGFILE
+        sudo kill $PIDS >> $LOG_FILE
+        sudo kill -9 $PIDS >> $LOG_FILE
     fi
 
     echo 64
     sleep 0.25
 
     # Execute the mlat-client maintenance script.
-    sudo nohup $PWD/adsbexchange-mlat_maint.sh > /dev/null 2>&1 & >> $LOGFILE
+    sudo nohup $PWD/adsbexchange-mlat_maint.sh > /dev/null 2>&1 & >> $LOG_FILE
 
     echo 70
     sleep 0.25
@@ -226,7 +237,7 @@ EOF
     sleep 0.25
 
     # Set permissions on the file adsbexchange-netcat_maint.sh.
-    chmod +x adsbexchange-netcat_maint.sh >> $LOGFILE
+    chmod +x adsbexchange-netcat_maint.sh >> $LOG_FILE
 
     echo 82
     sleep 0.25
@@ -234,7 +245,7 @@ EOF
     # Add a line to execute the netcat maintenance script to /etc/rc.local so it is started after each reboot if one does not already exist.
     if ! grep -Fxq "$PWD/adsbexchange-netcat_maint.sh &" /etc/rc.local; then
         lnum=($(sed -n '/exit 0/=' /etc/rc.local))
-        ((lnum>0)) && sudo sed -i "${lnum[$((${#lnum[@]}-1))]}i $PWD/adsbexchange-netcat_maint.sh &\n" /etc/rc.local >> $LOGFILE
+        ((lnum>0)) && sudo sed -i "${lnum[$((${#lnum[@]}-1))]}i $PWD/adsbexchange-netcat_maint.sh &\n" /etc/rc.local >> $LOG_FILE
     fi
 
     echo 88
@@ -243,21 +254,21 @@ EOF
     # Kill any currently running instances of the adsbexchange-netcat_maint.sh script.
     PIDS=`ps -efww | grep -w "adsbexchange-netcat_maint.sh" | awk -vpid=$$ '$2 != pid { print $2 }'`
     if [ ! -z "$PIDS" ]; then
-        sudo kill $PIDS >> $LOGFILE
-        sudo kill -9 $PIDS >> $LOGFILE
+        sudo kill $PIDS >> $LOG_FILE
+        sudo kill -9 $PIDS >> $LOG_FILE
     fi
 
     echo 94
     sleep 0.25
 
-    # Execute the netcat maintenance script.
-    sudo nohup $PWD/adsbexchange-netcat_maint.sh > /dev/null 2>&1 & >> $LOGFILE
+    # Start netcat script.
+    sudo nohup $PWD/adsbexchange-netcat_maint.sh > /dev/null 2>&1 & >> $LOG_FILE
     echo 100
     sleep 0.25
 
-} | whiptail --backtitle "$BACKTITLETEXT" --title "Setting Up ADS-B Exchange Feed"  --gauge "\nSetting up your receiver to feed ADS-B Exchange.\nThe setup process may take awhile to complete..." 8 60 0
+} | whiptail --backtitle "$RECEIVER_PROJECT_TITLE" --title "Setting Up ADS-B Exchange Feed"  --gauge "\nSetting up your receiver to feed ADS-B Exchange.\nThe setup process may take awhile to complete..." 8 60 0
 
-## SETUP COMPLETE
+### SETUP COMPLETE
 
 # Display the thank you message box.
 whiptail --title "ADS-B Exchange Setup Script" --msgbox "\nSetup is now complete.\n\nYour feeder should now be feeding data to ADS-B Exchange.\nThanks again for choosing to share your data with ADS-B Exchange!\n\nIf you have questions or encountered any issues while using this script feel free to post them to one of the following places.\n\nhttps://github.com/jprochazka/adsb-exchange\nhttp://www.adsbexchange.com/forums/topic/ads-b-exchange-setup-script/" 17 73
